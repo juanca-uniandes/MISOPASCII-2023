6bbd1ca5c1a24ebcfac2d06c1873d1e45fe762aa
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2023-11-12 11:52:44 -0500

    Adicionar pagina cypress

---
+++ e2e/cypress/e2e/create_page.js
@@ -0,0 +1,36 @@
+import './login.cy';
+require('@cypress/xpath');
+import { CREATE_NEW_PAGE_OPTION, PAGES_SECTION,  PAGE_TITLE_TEXTAREA, BODY_TEXT, PUBLISH_OPTION, FINISH_REVIEW_BUTTON, PUBLISH_PAGE_NOW_BUTTON, CONFIRMATION_ELEMENT } from './globals/constants';
+
+Cypress.Commands.add('crearPagina', () => {
+  cy.hacerLoginCorrecto();
+
+  cy.xpath(PAGES_SECTION).click();
+  cy.wait(2000);
+
+  cy.xpath(CREATE_NEW_PAGE_OPTION).click();
+  cy.wait(2000);
+
+  cy.xpath(PAGE_TITLE_TEXTAREA).type('Sample Page');
+  cy.wait(2000);
+
+  cy.xpath(BODY_TEXT).click();
+  cy.wait(2000);
+
+  cy.xpath(PUBLISH_OPTION).click();
+  cy.wait(2000);
+
+  cy.xpath(FINISH_REVIEW_BUTTON).click();
+  cy.wait(2000);
+
+  cy.xpath(PUBLISH_PAGE_NOW_BUTTON).click();
+  cy.wait(5000);
+});
+
+describe('User can create page successfully', () => {
+  it('should create a page successfully', () => {
+    cy.crearPagina();
+    cy.xpath(CONFIRMATION_ELEMENT).should('exist');
+    cy.wait(2000);
+  });
+});
---
+++ e2e/cypress/e2e/create_post.js
@@ -0,0 +1,27 @@
+import './login.cy';
+require('@cypress/xpath');
+const { BUTTON_SEE_MOST_POST, BUTTON_CREATE_NEW_POST, INPUT_POST_TITLE, INPUT_BODY_TEXT, BUTTON_PUSBLISH_POST, BUTTON_FINISH_POST_REVIEW, BUTTON_PUBLISH_POST_NOW, DIV_TEXT_WHEN_POST_PAGE_TAG_ARE_CREATED } = require('./globals/constants');
+
+Cypress.Commands.add('crearPublicacion', () => {
+  cy.hacerLoginCorrecto();
+  cy.xpath(BUTTON_SEE_MOST_POST).click();
+  cy.wait(1000);
+  cy.xpath(BUTTON_CREATE_NEW_POST).click();
+  cy.wait(1000);
+  cy.xpath(INPUT_POST_TITLE).type('New Post Title');
+  cy.wait(1000);
+  cy.xpath(BUTTON_PUSBLISH_POST).click();
+  cy.wait(1000);
+  cy.xpath(BUTTON_FINISH_POST_REVIEW).click();
+  cy.wait(1000);
+  cy.xpath(BUTTON_PUBLISH_POST_NOW).click();
+  cy.wait(2000);
+});
+
+describe('Como usuario quiero crear una publicación exitosamente', () => {
+  it('Crear una publicación exitosamente', () => {
+    cy.crearPublicacion();
+    cy.screenshot(`create_post_exitoso`);
+    cy.xpath(DIV_TEXT_WHEN_POST_PAGE_TAG_ARE_CREATED).should('exist');
+  });
+});
