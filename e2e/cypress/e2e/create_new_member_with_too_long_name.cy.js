2aa6f9e6db3757f3085abb2a01b1be4e3f284518
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2023-11-23 21:58:04 -0500

    Adicionar test para miembro

---
+++ e2e/cypress/e2e/create_new_member_with_too_long_name.cy.js
@@ -0,0 +1,27 @@
+import {faker} from '@faker-js/faker';
+import './login_with_correct_credentials.cy';
+require('@cypress/xpath');
+describe('Como usuario quiero verificar si puedo crear un miembro con un nombre muy largo', () => {
+
+    it('Intentar crear miembro con nombre muy largo', () => {
+        cy.hacerLoginCorrecto();
+        const fakeEmail = faker.internet.email();
+        const fakeName = faker.lorem.words(1000);
+        cy.xpath('/html/body/div[2]/div/nav[1]/div/section/div[1]/ul[2]/li[4]/a').click();
+        cy.wait(1000);
+        cy.xpath('/html/body/div[2]/div/main/section/div/header/section/div[2]/a').click();
+        cy.wait(1000);
+        cy.xpath('/html/body/div[2]/div/main/section/div[2]/form/div/section/div/div[1]/div/div[1]/div[1]/input').type(fakeName);
+        cy.wait(1000);
+        cy.xpath('/html/body/div[2]/div/main/section/div[2]/form/div/section/div/div[1]/div/div[1]/div[2]/input').type(fakeEmail);
+        cy.wait(1000);
+        cy.xpath('/html/body/div[2]/div/main/section/div[1]/header/section/button').click();
+        cy.wait(1000);
+        cy.screenshot(`create_member_with_too_long_name`);
+        cy.xpath('/html/body/div[2]/div/main/section/div[1]/header/section/button/span').invoke('text').then((actualText) => {
+            const cleanedActualText = actualText.replace(/\s+/g, ' ').trim();
+            expect(cleanedActualText).to.equal('Retry');
+        });
+        cy.wait(1000);
+    });
+});
